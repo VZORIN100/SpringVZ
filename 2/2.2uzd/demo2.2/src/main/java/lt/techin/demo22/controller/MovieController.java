4beabb5f4c2cbdda65088c9e1ffd4a6583e93981
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-29 16:25:57 +0400

    2.2uzd.demo2.2 Exercise complete.

---
+++ 2/2.2uzd/demo2.2/src/main/java/lt/techin/demo22/controller/MovieController.java
@@ -0,0 +1,56 @@
+package lt.techin.demo22.controller;
+
+import lt.techin.demo22.model.Movie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class MovieController {
+
+  private List<Movie> movies = new ArrayList<>(
+          List.of(
+                  new Movie(1, "One Bird", "A"),
+                  new Movie(2, "Two Bird", "B"),
+                  new Movie(3, "Three Bird", "C")
+          )
+  );
+
+  @GetMapping("/movies")
+  public ResponseEntity<List<Movie>> getBooks() {
+    return ResponseEntity.ok(movies);
+  }
+
+  @GetMapping("/movies/{index}")
+  public ResponseEntity<Movie> getMovie(@PathVariable int index) {
+    if (index > movies.size() - 1) {
+      return ResponseEntity.notFound().build();// build() reikia nurodyti, nes jokio kūno negrąžiname
+    }
+
+    return ResponseEntity.ok(movies.get(index));
+  }
+
+  @PostMapping("/movies")
+  public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie) {
+    // Jei pavadinimas arba autorius tušti, grąžinti 400 Bad Request
+    if (movie.getTitle().isEmpty() || movie.getDirector().isEmpty()) {
+      return ResponseEntity.badRequest().build();
+    }
+
+    movies.add(movie);
+
+    return ResponseEntity.created(
+                    ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{index}")
+                            .buildAndExpand(movies.size() - 1)
+                            .toUri())
+            .body(movie);
+  }
+
+}
