5583b8cae8a87e1c1c231de309361cec2b5eb93b
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-01-30 19:33:50 +0400

    3.2uzd.demo2.2; 3.2uzd complete. 3.2uzd code added to demo2.2.

---
+++ 3/3.2uzd/demo2.2/src/main/java/lt/techin/demo22/controller/MovieController.java
@@ -0,0 +1,141 @@
+package lt.techin.demo22.controller;
+
+import lt.techin.demo22.model.Movie;
+import lt.techin.demo22.repository.MovieRepository;
+import lt.techin.demo22.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class MovieController {
+
+//  private List<Movie> movies = new ArrayList<>(
+//          List.of(
+//                  new Movie(1, "One Cat", "A1"),
+//                  new Movie(2, "Two Cat", "B1"),
+//                  new Movie(3, "Three Cat", "C1")
+//          )
+//  );
+
+  private final MovieService movieService;
+
+  @Autowired // constructor injection (very good)
+  public MovieController(MovieService movieService) {
+    this.movieService = movieService;
+  }
+
+  @GetMapping("/movies")
+  public ResponseEntity<List<Movie>> getMovies() {
+    return ResponseEntity.ok(movieService.findAllMovies());
+  }
+
+  @GetMapping("/movies/{id}")
+  public ResponseEntity<Movie> getMovie(@PathVariable long id) {
+//    if (id > movies.size() - 1) {
+//      return ResponseEntity.notFound().build();// build() reikia nurodyti, nes jokio kūno negrąžiname
+//    }
+    Optional<Movie> foundMovie = movieService.findMovieById(id);
+
+    if (foundMovie.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(foundMovie.get());
+  }
+
+  @PostMapping("/movies")
+  // Klaustukas - būtinas, nes grąžinamas tipas gali būti
+  // Book arba String
+  public ResponseEntity<?> saveMovie(@RequestBody Movie movie) {
+    // Jei pavadinimas arba autorius tušti, grąžinti 400 Bad Request
+    if (movie.getTitle().isEmpty() || movie.getDirector().isEmpty()) {
+      //      return ResponseEntity.badRequest().build();
+      // Grąžiname ir String, tam kad klientui būdų aiškiau,
+      // kas įvyko blogai
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Title or director cannot be empty");
+    }
+
+    Movie savedMovie = movieService.saveMovie(movie);
+
+    return ResponseEntity.created(
+                    ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(savedMovie.getId())
+                            .toUri())
+            .body(movie);
+  }
+
+  // Toks endpoint geriau
+  @GetMapping("/movies/search") // /books neveiks, nes užimta
+  public ResponseEntity<Movie> getMovieByTitle(@RequestParam String title) {
+//    Optional<Movie> foundMovie = movieService.findAllMovies().stream()
+//            .filter(b -> movieService.existsMovieByTitle(title))
+//            .findFirst();
+
+    // Reikia koreguoti
+
+    for (Movie movie : movieService.findAllMovies()) {
+      if (!movieService.existsMovieByTitle(title)) {
+        return ResponseEntity.notFound().build();
+      }
+    }
+
+    return ResponseEntity.ok(movieService.findMovieByTitle(title));
+  }
+
+  @PutMapping("/movies/{id}")
+  public ResponseEntity<?> updateMovie(@PathVariable long id, @RequestBody Movie movie) {
+    if (movie.getTitle().isEmpty() || movie.getDirector().isEmpty()) {
+      //return ResponseEntity.badRequest().build();
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Title or director cannot be empty");
+    }
+
+    // Tikriname, ar paduotas indeksas id yra???
+    if (movieService.existsMovieById(id)) {
+      Movie movieFromDb = movieService.findMovieById(id).get();
+
+      movieFromDb.setTitle(movie.getTitle());
+      movieFromDb.setDirector(movie.getDirector());
+
+      return ResponseEntity.ok(movieService.saveMovie(movieFromDb));
+    }
+
+    Movie savedMovie = movieService.saveMovie(movie);
+
+    return ResponseEntity.created(
+                    ServletUriComponentsBuilder.fromCurrentRequest()
+                            // Reikia daryti replacePath, nes kitap priklijuos rezultatą prie
+                            // egzistuojančio galo
+                            .replacePath("/api/movies/{id}")
+                            .buildAndExpand(savedMovie.getId())
+                            .toUri())
+            .body(movie);
+  }
+
+  @DeleteMapping("/movies/{id}")
+  public ResponseEntity<Void> deleteMovie(@PathVariable long id) {
+    // Pastebime, jog tikrinimo sąkinius rašome viršuje. Tai vadinama Guard Clauses
+//    if (index > movies.size() - 1) {
+//      return ResponseEntity.notFound().build();
+//    }
+//
+//    movies.remove(index);
+    if (!movieService.existsMovieById(id)) {
+      return ResponseEntity.notFound().build();
+    }
+
+    movieService.deleteMovieById(id);
+    return ResponseEntity.noContent().build();
+  }
+
+
+}
---
+++ 3/3.2uzd/demo2.2/src/main/java/lt/techin/demo22/repository/MovieRepository.java
@@ -0,0 +1,16 @@
+package lt.techin.demo22.repository;
+
+import lt.techin.demo22.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// Tipas klasės, ir jos pirminis raktas <Movie, Long>
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+  // Derived query
+  boolean existsByTitle(String title);
+
+  // Derived query
+  Movie findByTitle(String title);
+}
