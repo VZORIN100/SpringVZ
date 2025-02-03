a2f8ae2f0d293727dc1e803f42fd2e9f92c9135d
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-02-03 20:06:54 +0400

    4.1uzd folder; demo; exercise complete.

---
+++ 4/4.1/4.1uzd/demo/src/main/java/lt/techin/demo/model/Movie.java
@@ -0,0 +1,75 @@
+package lt.techin.demo.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "movies") // Jei to neparašysiu, lentelė bus traktuojama kaip „book“
+public class Movie {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // SQL kode, tai yra AUTO_INCREMENT
+  private long id; // Duomenų bazėje: BIGINT
+
+  private String title; // Duomenų bazėje: VARCHAR
+  private String director; // VARCHAR
+
+  // Galiu ir vaikinėje lentelėje išsaugoti įrašą - review
+  @OneToMany(cascade = CascadeType.ALL)
+
+  // Neuždėjus šios anotacijos, Hibernate bandys pats kurti tarpinę lentelę
+  @JoinColumn(name = "movie_id")
+  private List<Screening> screenings;
+
+
+  // id nereikia, nes jis generuojamas
+  public Movie(String title, String director, List<Screening> screenings) {
+    //this.id = id;
+    this.title = title;
+    this.director = director;
+    this.screenings = screenings;
+  }
+
+
+  // Konstruktorius be argumentų yra reikalingas,
+  // tam kad Hibernate galėtų veikti
+  public Movie() {
+
+  }
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDirector() {
+    return director;
+  }
+
+  public void setDirector(String director) {
+    this.director = director;
+  }
+
+  // getId() reikalingas, kad Jackson galėtu serialize/
+  // deserialize id
+  public long getId() {
+    return id;
+  }
+
+  public List<Screening> getScreenings() {
+    return screenings;
+  }
+
+  public void setScreenings(List<Screening> screenings) {
+    this.screenings = screenings;
+  }
+
+}
---
+++ 4/4.1/4.1uzd/demo/src/main/java/lt/techin/demo/model/Screening.java
@@ -0,0 +1,59 @@
+package lt.techin.demo.model;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "screenings")
+public class Screening {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String theatre;
+  private LocalDate date;
+  private LocalTime time;
+
+
+  public Screening(String theatre, LocalDate date, LocalTime time) {
+    this.theatre = theatre;
+    this.date = date;
+    this.time = time;
+  }
+
+  public Screening() {
+  }
+
+  public String getTheatre() {
+    return theatre;
+  }
+
+  public void setTheatre(String theatre) {
+    this.theatre = theatre;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public LocalTime getTime() {
+    return time;
+  }
+
+  public void setTime(LocalTime time) {
+    this.time = time;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+}
