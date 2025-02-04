fb0dee6d40d3f9e42413355a99be94667135e061
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-02-04 16:59:25 +0400

    5.2uzd folder; file name: demo; exercise complete.

---
+++ 5/5.2uzd/demo/src/main/java/lt/techin/demo/repository/MovieRepository.java
@@ -0,0 +1,37 @@
+package lt.techin.demo.repository;
+
+import lt.techin.demo.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+// Tipas klasės, ir jos pirminis raktas <Movie, Long>
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+  // Derived query
+  List<Movie> findAllByTitleContaining(String title);
+
+  // Šis metodas ras filmus, jei pavadinimas yra vienas prie vieno
+  List<Movie> findAllByTitle(String title);
+
+  // ?1 simbolizuoja pirmą metodo parametrą, t. y. director
+  @NativeQuery(value = "SELECT * FROM movies WHERE director = ?1")
+  List<Movie> findAllByDirector(String director);
+
+  // JPQL query; veikia taip pat, kaip @NativeQuery
+  // Sintaksė remiasi būtent Java kodu
+  // @Query("select b from Book b where b.author = ?1")
+  // List<Book> findAllByAuthor(String author);
+
+  //Previous 3.2uzd.
+//  // Derived query
+//  boolean existsByTitle(String title);
+//
+//  // Derived query
+//  Movie findByTitle(String title);
+}
