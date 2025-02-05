9a6cbfcb64b2e8cf0d75ab2b89bb56151341e537
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-02-05 12:19:54 +0400

    5.3uzd folder; file name: demo; half complete.

---
+++ 5/5.3uzd/demo/src/main/java/lt/techin/demo/validation/TheatreValidator.java
@@ -0,0 +1,13 @@
+package lt.techin.demo.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class TheatreValidator implements ConstraintValidator<Theatre, String> {
+
+  @Override
+  public boolean isValid(String theatre, ConstraintValidatorContext constraintValidatorContext) {
+    return theatre != null && theatre.matches("^[A-Z][a-z]+$")
+            && (theatre.length() >= 4) && (theatre.length() <= 150);
+  }
+}
